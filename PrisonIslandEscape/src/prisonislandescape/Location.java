07fe9b70e61f5b84f069077eef8a3ed4b5da38dc
StuckFanatic <157004704+StuckFanatic@users.noreply.github.com>
2026-02-23 21:21:21 -0600

    Create Location.java

---
+++ PrisonIslandEscape/src/prisonislandescape/Location.java
@@ -0,0 +1,57 @@
+package prisonislandescape;
+
+import java.util.HashMap;
+import java.util.Map;
+
+
+public class Location {
+
+	private String name;
+	private String description;
+	
+	
+	//Holds possible ways to leave locations
+	private Map<String, Location> exits;
+	
+	//Allows the player using Map<String...> to go to each location making the game semi-open world
+	public Location(String name, String description) {
+		
+		this.name = name;
+		this.description = description;
+		this.exits = new HashMap<>();
+		
+	}
+	
+	public String getName() {
+		
+		return name;
+		
+	}
+	
+	public String getDescription() {
+		
+		return description;
+		
+	}
+	
+	public void addExit(String direction, Location location) {
+		
+		exits.put(direction.toLowerCase(), location);
+		
+		
+	}
+	
+	public Location getExit(String direction) {
+		
+		return exits.get(direction.toLowerCase());
+		
+	}
+	
+	public String getAvailableExits() {
+		
+		return exits.keySet().toString();
+		
+	}
+	
+	
+}
